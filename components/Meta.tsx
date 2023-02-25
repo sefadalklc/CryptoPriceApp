d99209a6320b870454b4d04616046f7470051d9a
sefadalklc <sefadalk11@gmail.com>
2023-02-25 09:58:16 +0300

    updated

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 function Meta() {
   return (
     <Head>
-      <title>Crypto Price Tracker App | Code of Relevancy</title>
+      <title>Crypto Price Tracker App </title>
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
